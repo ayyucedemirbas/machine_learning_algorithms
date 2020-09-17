5789d94713f98a4977033768df1c0519a617e5e3
Ayyuce Demirbas <ayyucedemirbas@windowslive.com>
2020-09-17 20:37:07 +0300

    Add files via upload

---
+++ knn_from_scratch.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 20:33:00 2020
+
+https://machinelearningmastery.com/tutorial-to-implement-k-nearest-neighbors-in-python-from-scratch/
+@author: ayyuce
+"""
+
+# Make Predictions with k-nearest neighbors on the Iris Flowers Dataset
+from csv import reader
+from math import sqrt
+
+# Load a CSV file
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+	class_values = [row[column] for row in dataset]
+	unique = set(class_values)
+	lookup = dict()
+	for i, value in enumerate(unique):
+		lookup[value] = i
+		print('[%s] => %d' % (value, i))
+	for row in dataset:
+		row[column] = lookup[row[column]]
+	return lookup
+
+# Find the min and max values for each column
+def dataset_minmax(dataset):
+	minmax = list()
+	for i in range(len(dataset[0])):
+		col_values = [row[i] for row in dataset]
+		value_min = min(col_values)
+		value_max = max(col_values)
+		minmax.append([value_min, value_max])
+	return minmax
+
+# Rescale dataset columns to the range 0-1
+def normalize_dataset(dataset, minmax):
+	for row in dataset:
+		for i in range(len(row)):
+			row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
+# Calculate the Euclidean distance between two vectors
+def euclidean_distance(row1, row2):
+	distance = 0.0
+	for i in range(len(row1)-1):
+		distance += (row1[i] - row2[i])**2
+	return sqrt(distance)
+
+# Locate the most similar neighbors
+def get_neighbors(train, test_row, num_neighbors):
+	distances = list()
+	for train_row in train:
+		dist = euclidean_distance(test_row, train_row)
+		distances.append((train_row, dist))
+	distances.sort(key=lambda tup: tup[1])
+	neighbors = list()
+	for i in range(num_neighbors):
+		neighbors.append(distances[i][0])
+	return neighbors
+
+# Make a prediction with neighbors
+def predict_classification(train, test_row, num_neighbors):
+	neighbors = get_neighbors(train, test_row, num_neighbors)
+	output_values = [row[-1] for row in neighbors]
+	prediction = max(set(output_values), key=output_values.count)
+	return prediction
+
+# Make a prediction with KNN on Iris Dataset
+filename = '_iris.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])-1):
+	str_column_to_float(dataset, i)
+# convert class column to integers
+str_column_to_int(dataset, len(dataset[0])-1)
+# define model parameter
+num_neighbors = 5
+# define a new record
+row = [5.7,2.9,4.2,1.3]
+# predict the label
+label = predict_classification(dataset, row, num_neighbors)
+print('Data=%s, Predicted: %s' % (row, label))
